9f130b726452b9748c3dc4254e3ce11c3c1cfaf6
devsalman247 <salman247.dev@gmail.com>
2022-12-29 03:18:16 +0500

    some fixes

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import { Web3Auth } from "@web3auth/modal";
+import Web3 from "web3";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+	const [address, setAddress] = useState("");
+	const [web3auth, setWeb3auth] = useState(null);
+
+	const handleSubmit = async () => {
+		// const web3auth = new Web3Auth({
+		// 	clientId: "BD3vOjiwGiSFmmJ59O_sk3_g26oRtYnmn3OPNN7DmhWuZppFypQY2ETVWH8bMTRlPWtCRC0im1hkqNBHLODvFLw",
+		// 	chainConfig: {
+		// 		chainNamespace: "eip155",
+		// 		chainId: "0x1",
+		// 		rpcTarget: "https://rpc.ankr.com/eth",
+		// 		displayName: "Ethereum Mainnet",
+		// 		blockExplorer: "https://etherscan.io/",
+		// 		ticker: "ETH",
+		// 		tickerName: "Ethereum",
+		// 	},
+		// });
+
+		// setWeb3auth(web3auth);
+		// await web3auth.initModal();
+
+		if (!web3auth) {
+			console.log("web3auth not initialized yet");
+			return;
+		}
+		const web3authProvider = await web3auth.connect();
+		const web3 = new Web3(web3authProvider);
+		const userAccounts = await web3.eth.getAccounts();
+		setAddress(userAccounts[0]);
+		// console.log(userAccounts);
+	};
+
+	useEffect(() => {
+		localStorage.clear();
+		const init = async () => {
+			try {
+				const web3auth = new Web3Auth({
+					clientId: "BD3vOjiwGiSFmmJ59O_sk3_g26oRtYnmn3OPNN7DmhWuZppFypQY2ETVWH8bMTRlPWtCRC0im1hkqNBHLODvFLw",
+					chainConfig: {
+						chainNamespace: "eip155",
+						chainId: "0x1",
+						rpcTarget: "https://rpc.ankr.com/eth",
+						displayName: "Ethereum Mainnet",
+						blockExplorer: "https://etherscan.io/",
+						ticker: "ETH",
+						tickerName: "Ethereum",
+					},
+				});
+				setWeb3auth(web3auth);
+				await web3auth.initModal();
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		init();
+	}, []);
+
+	return (
+		<div className="App">
+			<button onClick={handleSubmit}>Click to login</button>
+			{address && (
+				<>
+					<p>Connected: true</p>
+					<p>Your wallet: {address}</p>
+				</>
+			)}
+		</div>
+	);
+}
+
+export default App;
