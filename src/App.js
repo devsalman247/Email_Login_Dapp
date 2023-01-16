caa1a3886742db82f983ec7c94b3e058e2cc8166
devsalman247 <salman247.dev@gmail.com>
2023-01-16 23:35:28 +0500

    some fixes

--- src/App.js
+++ src/App.js
@@ -646,7 +646,7 @@ function App() {
 					displayName: "Binance SmartChain Testnet",
 					blockExplorer: "https://testnet.bscscan.com",
 					ticker: "BNB",
-					tickerName: "BNB",
+					tickerName: "ETH",
 
 					// Ethereum Goerli Testnet
 
